286e6729be3ba584ee2a748820add8c8ab85cad1
Neil Murphy <barrymun@tcd.ie>
2023-12-03 17:44:14 +0000

    adding comments to the getPeriod function so that it can be better understood, performing more thorough testing of this function, linting the tests directory as well as src, adding the test suite to the pre-commit hook

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,29 @@
 import { flipFrequency, pitchAmplitude, pitchFrequency, rollFrequency } from "utils/constants";
 import { CraftManoeuvre } from "utils/types";
 
+/**
+ * The given code calculates the period (T) of a periodic function based on the frequency (f) using the formula:
+ *
+ * T = (2 * π) / |f|
+ *
+ * Here's what each part of the formula does:
+ *
+ * 2 * Math.PI: This part of the formula calculates 2 times the mathematical constant π (pi),
+ * which is approximately equal to 3.14159. It essentially represents two times the circumference of a circle.
+ *
+ * Math.abs(frequency): This part calculates the absolute value of the frequency (f).
+ * The absolute value ensures that the result is always positive,
+ * regardless of whether the frequency is positive or negative.
+ *
+ * (2 * Math.PI) / Math.abs(frequency): This expression divides the value obtained from doubling π by the
+ * absolute value of the frequency. The result of this division represents the period (T) of the periodic function.
+ *
+ * In summary, the code calculates the period of a periodic function based on the given frequency,
+ * ensuring that the result is always positive.
+ *
+ * @param frequency
+ * @returns
+ */
 export const getPeriod = (frequency: number): number => {
   return (2 * Math.PI) / Math.abs(frequency);
 };
--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,6 +1,14 @@
-import { expect, test } from 'vitest';
-import { getPeriod } from '../src/utils/helpers';
+import { expect, test } from "vitest";
+import { getPeriod } from "../src/utils/helpers";
 
-test('getPeriod', () => {
-  expect(getPeriod(1)).toBe(6.283185307179586)
+test("getPeriod(1)", () => {
+  expect(getPeriod(1)).toBe(6.283185307179586);
+});
+
+test("getPeriod(2)", () => {
+  expect(getPeriod(2)).toBe(3.141592653589793); // pi
+});
+
+test("getPeriod(3)", () => {
+  expect(getPeriod(3)).toBe(2.0943951023931953);
 });
